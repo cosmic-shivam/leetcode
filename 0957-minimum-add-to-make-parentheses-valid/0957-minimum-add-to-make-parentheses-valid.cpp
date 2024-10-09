19a2b54ef2ee94fb41b4e4ee08f091da6da7137b
cosmic <119277577+cosmic-shivam@users.noreply.github.com>
2024-10-09 12:46:03 +0530

    Time: 3 ms (42.94%), Space: 7.8 MB (81.46%) - LeetHub

--- 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cpp
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cpp
@@ -3,7 +3,7 @@ public:
     int minAddToMakeValid(string s) {
         int open=0;
         int close=0;
-        int count=0;
+        // int count=0;
         for(char ch:s)
         {
             if(ch=='(')open++;
