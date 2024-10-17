15ce69fad0db9f6af322a425bd9b53a11e60248f
cosmic <119277577+cosmic-shivam@users.noreply.github.com>
2024-10-17 15:18:54 +0530

    Time: 0 ms (100%), Space: 7.4 MB (89.76%) - LeetHub

--- 0670-maximum-swap/0670-maximum-swap.cpp
+++ 0670-maximum-swap/0670-maximum-swap.cpp
@@ -1,30 +1,40 @@
 class Solution {
 public:
-//Take array and store the index of maximum element of right hand side
+//Take array and store the index of maximum element of right hand side   // better approach O(2n) Space: O(n)
+
+// Optimal Approach : In this we just simply take two index which might get swap , and maxindex and its max Element now just check from backside weather current element is greater than previous then it's our next maxelemnet and store it's index now else this is our position which might get swap . but we want last position which will get swap .
     int maximumSwap(int num) {
         string s=to_string(num);
         int n=s.size();
-
-        vector<int>ans(n,-1);
-        ans[n-1]=n-1 ;
-
+        char maxEle=s[n-1];
+        int maxInd=n-1;
+        int swapInd1=-1;
+        int swapInd2=-1;
+        // vector<int>ans(n,-1);
+        // ans[n-1]=n-1 ;
         for(int i=n-2;i>=0;i--)
         {
-            if(s[i]-'0'>s[ans[i+1]]-'0')
+            if(s[i]-'0'>maxEle-'0')
             {
-                ans[i]=i;
+                maxEle=s[i];
+                maxInd=i;
             }
-            else{
-                ans[i]=ans[i+1];
+            else if (s[i]-'0'<maxEle-'0'){
+                swapInd1=i;
+                swapInd2=maxInd;
             }
         }
-        for(int i=0;i<n;i++)
+        if(swapInd1!=-1)
         {
-            if(s[i]<s[ans[i]]){
-                swap(s[i],s[ans[i]]);
-                break;
-            }
+            swap(s[swapInd1],s[swapInd2]);
         }
+        // for(int i=0;i<n;i++)
+        // {
+        //     if(s[i]<s[ans[i]]){
+        //         swap(s[i],s[ans[i]]);
+        //         break;
+        //     }
+        // }
 
         return stoi(s);
         
