41392475a319a946dd6dd65a76c7757f67156e23
cosmic <119277577+cosmic-shivam@users.noreply.github.com>
2024-10-22 12:20:54 +0530

    Time: 3 ms (97.71%), Space: 14.6 MB (17.27%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,13 +1,25 @@
 class Solution {
 public:
-   vector<int> twoSum(vector<int>& nums, int target) {
-    for (int i = 0; i < nums.size(); i++) {
-        for (int j = i + 1; j < nums.size(); j++) {
-            if (nums[i] + nums[j] == target) {
-                return {i, j};
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n=nums.size();
+        int sum=0;
+        int x=0;
+        map<int,int>mpp;
+        for(int i=0;i<n;i++)
+        {
+            x=target-nums[i];
+            if(mpp.find(x)!=mpp.end())
+            {
+                return {mpp[x],i};
             }
+            mpp[nums[i]]=i;
+
+
         }
+
+        
+
+        return {};
+        
     }
-    return {}; // No solution found!
-}
 };
