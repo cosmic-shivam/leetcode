1ee26051f55680c7fe320111d6c3e7b55297b5c4
cosmic <119277577+cosmic-shivam@users.noreply.github.com>
2024-11-02 21:02:06 +0530

    Time: 2 ms (16.75%), Space: 7.8 MB (62.83%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+
+    int cal(int m,int n, vector<vector<int>>&dp)
+    {
+        if(m==0 && n==0)return 1;
+        if(m<0 || n<0)return 0;
+
+        if(dp[m][n]!=-1)return dp[m][n];
+
+        int up=cal(m-1,n,dp);
+        int left=cal(m,n-1,dp);
+
+        return dp[m][n]=up+left;
+    }
+    int uniquePaths(int m, int n) {
+
+        vector<vector<int>>dp(m,vector<int>(n,-1));
+
+        return cal(m-1,n-1,dp);
+        
+    }
+};
