21b70338c8f4bfb75a563919465eae8b1ab128f9
cosmic <119277577+cosmic-shivam@users.noreply.github.com>
2024-12-14 22:57:45 +0530

    Time: 3 ms (73.73%), Space: 10.8 MB (23.92%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -2,35 +2,32 @@ class Solution {
 public:
     int characterReplacement(string s, int k) {
         int hash[26]={0};
+        int maxfrq=0;
+        int left=0;
         int n=s.size();
-        int i=0,j=0;
-        int maxlen=0;
-        int maxf=0;
-        int count=0;
-        while(j<n)
+        int maxwin=0;
+
+        for(int j=0;j<n;j++)
         {
             hash[s[j]-'A']++;
-            maxf=max(maxf,hash[s[j]-'A']);
+            maxfrq=max(maxfrq,hash[s[j]-'A']);
 
-            while((j-i+1)-maxf>k)
-            {
-                hash[s[i]-'A']--;
-                maxf=0;
-                for(int i=0;i<25;i++){
-                    maxf=max(maxf,hash[i]);
-                }
-                i++;
-            }
-            if((j-i+1)-maxf<=k)
-            {
-                maxlen=max(maxlen,j-i+1);
-                j++;
-            }
-            
+            int windowlen=j-left+1;
 
+            if(windowlen-maxfrq>k)
+            {
+                hash[s[left]-'A']--;
+                left++;
+            }
+
+            windowlen=j-left+1;
+
+            maxwin=max(maxwin,windowlen);
 
         }
-        return maxlen;
+        return maxwin;
+
+
         
     }
 };
